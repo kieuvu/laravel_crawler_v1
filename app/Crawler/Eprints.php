2d4628d28a8197914ac8222869ecfd69d0563304
kieuvu <kieuvuvuvu@gmail.com>
2022-03-02 15:46:12 +0700

    first commit

---
+++ app/Crawler/Eprints.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Crawler;
+
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use App\Http\Controllers\Crawler\SaveData;
+use Symfony\Component\HttpClient\HttpClient;
+
+
+class Eprints
+{
+
+  private $domain = 'http://eprints.lse.ac.uk';
+
+  public function urlCrawler()
+  {
+    $url     = 'http://eprints.lse.ac.uk/view/year/2021.html';
+    $client  = new Client();
+    $crawler = $client->request('GET', $url);
+
+    $data = $crawler->filter('p > a')->each(function ($node) {
+      return $node->attr("href");
+    });
+
+    foreach ($data as $url) {
+      $saver = new SaveData();
+      $saver->saveUrl($url);
+      $this->dataCrawler($url);
+    }
+  }
+
+  public function dataCrawler($target)
+  {
+    $url     = $target;
+    $client  = new Client();
+    $crawler = $client->request('GET', $url);
+
+    $data = [];
+
+    $data["title"]         = $crawler->filter('.ep_tm_pagetitle')->text();
+    $data["content"]       = $crawler->filter('.ep_summary_content_main > p')->last()->text();
+    $data["author"]        = $crawler->filter('.ep_summary_content_main .person_name')->text();
+    $data['download_link'] = ($crawler->filter('a.ep_document_link')->count() > 0)
+      ? $crawler->filter('a.ep_document_link')->attr('href')
+      : "";
+
+    $saver = new saveData();
+    $saver->saveDocument($data);
+    $saver->updateStage($url, 1);
+  }
+}
---
+++ database/migrations/2022_03_01_035929_create_documents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string("title")->nullable('false');
+            $table->string("author")->nullable('false');
+            $table->text("content")->nullable('false');
+            $table->string('download_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
